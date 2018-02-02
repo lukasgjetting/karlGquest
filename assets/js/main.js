048322e8fcf4143e05c7e408eed995137b45df17
Lukas Gjetting <lukasgjetting@gmail.com>
2018-02-02 13:25:04 +0100

    Implement Birgitte Appel.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 class Game {
-  constructor(context) {
+  constructor(context, player) {
     this.context = context;
     this.intervalId = 0;
     this.entities = [];
     this.onKeyDownHandlers = {};
     this.keysDown = {};
+    this.player = player;
 
     window.onkeydown = e => this.keysDown[e.keyCode] = true;
     window.onkeyup = e => delete this.keysDown[e.keyCode];
@@ -15,15 +16,16 @@ class Game {
     ctx.fillRect(0, 0, 500, 500);
     
     for(let i = 0; i < this.entities.length; i++) {
+      this.entities[i].update(this.player);
       this.entities[i].render();
     }
 
-    console.log(JSON.stringify(this.keysDown));
+    this.player.update();
+    this.player.render();
+
 
     for(let id in this.onKeyDownHandlers) {
-      console.log("checking");
       if(this.keysDown[this.onKeyDownHandlers[id].keyCode]) {
-        console.log("!!");
         this.onKeyDownHandlers[id].handler();
       }
     }
@@ -61,13 +63,42 @@ class Entity {
     this.context.arc(this.x, this.y, this.size, 0, 2*Math.PI);
     this.context.fill();
   }
+
+  update(player) {
+    // Do nothing
+  }
+}
+
+class Enemy extends Entity {
+  constructor(x, y, size, color, context, vicinityRange, onPlayerEnterVicinity) {
+    super(x, y, size, color, context);
+    this.vicinityRange = vicinityRange;
+    this.onPlayerEnterVicinity = onPlayerEnterVicinity;
+  }
+
+  update(player) {
+    const margin = player.size + this.size + this.vicinityRange;
+    if(
+      player.x + margin > this.x &&
+      player.x - margin < this.x &&
+      player.y + margin > this.y &&
+      player.y - margin < this.y
+    ) {
+      this.onPlayerEnterVicinity();
+    }
+  }
 }
 
 const c = document.getElementById("game");
 const ctx = c.getContext("2d");
 
-const game = new Game(ctx);
 const player = new Entity(50, 50, 20, "#cecece", ctx);
+const game = new Game(ctx, player);
+
+const birgitteAppel = new Enemy(250, 250, 25, "red", ctx, 50, () => {
+  console.log("HEJ KARL");
+});
+
 
 game.addOnKeyDownHandler("playerLeft", 37, () => {
   player.x -= 3;
@@ -82,6 +113,6 @@ game.addOnKeyDownHandler("playerDown", 40, () => {
   player.y += 3;
 });
 
-game.entities.push(player);
+game.entities.push(birgitteAppel);
 
 game.start();
