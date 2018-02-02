b7b21fa8fb318d7411c0c1da827199ca46e55c04
Lukas Gjetting <lukasgjetting@gmail.com>
2018-02-02 13:15:08 +0100

    Add player movement.

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,16 +3,30 @@ class Game {
     this.context = context;
     this.intervalId = 0;
     this.entities = [];
+    this.onKeyDownHandlers = {};
+    this.keysDown = {};
+
+    window.onkeydown = e => this.keysDown[e.keyCode] = true;
+    window.onkeyup = e => delete this.keysDown[e.keyCode];
   }
 
   loop() {
-    ctx.fillStyle = "#ddd";
+    ctx.fillStyle = "#fff";
     ctx.fillRect(0, 0, 500, 500);
+    
     for(let i = 0; i < this.entities.length; i++) {
       this.entities[i].render();
     }
 
-    this.checkInput();
+    console.log(JSON.stringify(this.keysDown));
+
+    for(let id in this.onKeyDownHandlers) {
+      console.log("checking");
+      if(this.keysDown[this.onKeyDownHandlers[id].keyCode]) {
+        console.log("!!");
+        this.onKeyDownHandlers[id].handler();
+      }
+    }
   }
 
   start() {
@@ -22,6 +36,14 @@ class Game {
   stop() {
     clearInterval(this.intervalId);
     this.intervalId = 0;
+  }
+
+  addOnKeyDownHandler(id, keyCode, handler) {
+    this.onKeyDownHandlers[id] = { keyCode, handler };
+  }
+
+  removeOnKeyDownHandler(id) {
+    delete this.onKeyDownHandlers[id];
   }
 }
 
@@ -45,7 +67,20 @@ const c = document.getElementById("game");
 const ctx = c.getContext("2d");
 
 const game = new Game(ctx);
-const player = new Entity(50, 50, 20, "red", ctx);
+const player = new Entity(50, 50, 20, "#cecece", ctx);
+
+game.addOnKeyDownHandler("playerLeft", 37, () => {
+  player.x -= 3;
+});
+game.addOnKeyDownHandler("playerUp", 38, () => {
+  player.y -= 3;
+});
+game.addOnKeyDownHandler("playerRight", 39, () => {
+  player.x += 3;
+});
+game.addOnKeyDownHandler("playerDown", 40, () => {
+  player.y += 3;
+});
 
 game.entities.push(player);
 
